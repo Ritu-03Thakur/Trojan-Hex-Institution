0af710bf326ff79b10eabb2836a1c0c257385400
Ritu Thakur <105920415+Ritu-03Thakur@users.noreply.github.com>
2022-12-18 03:22:31 +0530

    Add files via upload

---
+++ The-Trojan-Hex-Institution-main/README.md
@@ -0,0 +1,6 @@
+# The-Trojan-Hex-Institution
+
+
+
+this is the PPT of trojan Hex Instituion website....
+https://gngrouporg-my.sharepoint.com/:p:/g/personal/ip210079_gngrouporg_onmicrosoft_com/EXSpZV5dnIFPkhBlo-CrXuoBOrOD56fCaUEixPchG-ACwQ?e=jKXSiq
---
+++ The-Trojan-Hex-Institution-main/yug/yug.js
@@ -0,0 +1,17 @@
+var textWrapper = document.querySelector('.typing');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.typing .letter',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 2250,
+        delay: (el, i) => 150 * (i + 1)
+    }).add({
+        targets: '.typing',
+        opacity: 0,
+        duration: 1000,
+        easing: 'easeOutExpo',
+        delay: 1000
+    });
